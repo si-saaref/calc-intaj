ae0684a49292e5eb572e049469ee7acb3b334ccb
saareff <achmadsyarif241@gmail.com>
2022-03-06 22:37:50 +0700

    feat: create addColumn func

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// GET DATA
 const getBaseData = () => {
 	const documentInput = document.querySelectorAll('input.baseData');
 	const data = [];
@@ -43,13 +44,16 @@ const getPlace4 = () => {
 	return data;
 };
 
-const displayResult = (resultData) => {
-	const resultText = document.querySelectorAll('span.resultNumb');
-	resultText.forEach((elem, id) => {
-		console.log(elem);
-		elem.innerHTML = resultData[id];
-	});
+const getDataColAndRow = () => {
+	const rowData = document.querySelector('tbody.data-group');
+	const columnDataTitle = document.querySelector('tr.title-group');
+	const columnDataBody = document.querySelectorAll('tr.h-max-content');
+	const totalRow = rowData.childElementCount;
+	const totalColumn = columnDataTitle.childElementCount - 1;
+	return { rowData, columnDataTitle, columnDataBody, totalRow, totalColumn };
 };
+
+// CREATE DOM
 
 const createNewRow = (totRow, totCol) => {
 	const newInput = document.createElement('input');
@@ -61,7 +65,7 @@ const createNewRow = (totRow, totCol) => {
 	newTd.appendChild(newInput);
 	newTr.appendChild(newTh);
 	newTr.appendChild(newTd);
-	for (let i = 1; i < totCol; i++) {
+	for (let i = 1; i < totCol; i++) {  
 		const newInput = document.createElement('input');
 		newInput.classList.add(`w-full`, `py-3`, `px-2`, `text-center`, `m${i}Data`);
 		const newTd = document.createElement('td');
@@ -74,10 +78,49 @@ const createNewRow = (totRow, totCol) => {
 
 const addRow = (e) => {
 	e.preventDefault();
-	const rowData = document.querySelector('tbody.data-group');
-	const totalColumn = document.querySelector('tr.title-group').childElementCount - 1;
-	const newRow = createNewRow(rowData.childElementCount, totalColumn);
-	rowData.appendChild(newRow);
+	const { rowData, totalColumn, totalRow } = getDataColAndRow();
+	if (totalRow < 10) {
+		const newRow = createNewRow(totalRow, totalColumn);
+		rowData.appendChild(newRow);
+	} else {
+    alert('Your maximum muaamil is just 10. Please be considered')
+  }
+};
+
+const arabListNumb = ['الخامس', 'السادس', 'السابع', 'ثامن', 'التاسع', 'العاشر'];
+const createNewColumn = (totalColumn) => {
+	const newTh = document.createElement('th');
+	newTh.innerText = `الموقع ${arabListNumb[totalColumn - 5]}`;
+	newTh.classList.add('w-2');
+	newTh.setAttribute('scope', 'col');
+	return { newTh };
+};
+
+const addColumn = (e) => {
+	e.preventDefault();
+	const { columnDataTitle, columnDataBody, totalColumn } = getDataColAndRow();
+	if (totalColumn <= 10) {
+		const { newTh } = createNewColumn(totalColumn);
+		columnDataTitle.appendChild(newTh);
+		columnDataBody.forEach((newCol) => {
+			const newTd = document.createElement('td');
+			const newInput = document.createElement('input');
+			newInput.classList.add(`w-full`, `py-3`, `px-2`, `text-center`, `m${totalColumn}Data`);
+			newTd.appendChild(newInput);
+			newCol.appendChild(newTd);
+		});
+	} else {
+    alert('Your maximum الموقع is 10. Please considered');
+  }
+};
+
+// Display Result
+const displayResult = (resultData) => {
+	const resultText = document.querySelectorAll('span.resultNumb');
+	resultText.forEach((elem, id) => {
+		console.log(elem);
+		elem.innerHTML = resultData[id];
+	});
 };
 
 const submitForm = document.getElementById('handleSubmit');
